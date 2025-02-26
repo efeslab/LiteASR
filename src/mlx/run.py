fe19fec804e5c91350a8b6175f9c3d68d4fbd8dc
Keisuke Kamahori <keisuke1258@gmail.com>
2025-02-26 15:13:57 -0800

    Import code

---
+++ src/mlx/run.py
@@ -0,0 +1,22 @@
+# Running inference for the compressed Whisper models using MLX.
+# This code is for demonstration purposes only.
+
+import argparse 
+
+import mlx_whisper
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, default="efficient-speech/lite-whisper-large-v3-turbo")
+    parser.add_argument("--audio-path", type=str, default="audio.wav")
+    args = parser.parse_args()
+
+    result = mlx_whisper.transcribe(
+        args.audio_path, 
+        path_or_hf_repo=args.model,
+        language="en",
+        temperature=0.0,
+        fp16=True,
+    )
+
+    print(result)
