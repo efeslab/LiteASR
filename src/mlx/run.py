822396b00d9bc7212167b70112bdd7f1a175c811
Rick Lan <hi@ricklan.net>
2025-04-10 12:44:35 +0900

    Refactor language as an argument

--- src/mlx/run.py
+++ src/mlx/run.py
@@ -9,12 +9,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="efficient-speech/lite-whisper-large-v3-turbo")
     parser.add_argument("--audio-path", type=str, default="audio.wav")
+    parser.add_argument("--language", type=str, default="en")
     args = parser.parse_args()
 
     result = mlx_whisper.transcribe(
         args.audio_path, 
         path_or_hf_repo=args.model,
-        language="en",
+        language=args.language,
         temperature=0.0,
         fp16=True,
     )
